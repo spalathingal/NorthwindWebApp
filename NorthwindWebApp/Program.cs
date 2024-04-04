c6d421f4438b41f0bf629948778c79775bdd5466
Stephen P <stephen.palathingal@gmail.com>
2024-04-03 21:24:54 -0400

    Basic swagger API

--- NorthwindWebApp/Program.cs
+++ NorthwindWebApp/Program.cs
@@ -1,5 +1,5 @@
-
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using NorthwindWebApp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +11,12 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<NorthwindDbContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("SQLiteConnection"));
+});
+
+// Add Swagger services
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
 });
 
 var app = builder.Build();
@@ -32,5 +38,12 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Enable middleware to serve generated Swagger as a JSON endpoint.
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+});
 
 app.Run();
